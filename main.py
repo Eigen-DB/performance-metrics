ea32425fcb6e289f3bea0e4d6f65d88eb8bcef21
Ryan Awad <ryanawad@yahoo.com>
2024-12-09 20:37:06 -0500

    Updated graph styling

--- main.py
+++ main.py
@@ -13,30 +13,30 @@ def generate_line_chart(csv_path: str, title: str, desc: str, x_label: str, y_la
         data=reshaped_df,
         x_label=x_label,
         y_label=y_label,
+        use_container_width=False,
+        width=700,
+        height=500
     )
 
 if __name__ == '__main__':
-    st.set_page_config(layout="wide") 
+    st.set_page_config() 
     st.markdown('''
     # EigenDB Performance Metrics
-    The data has been gathered using a [simple script](https://github.com/Eigen-DB/eigen-db/blob/main/benchmarks/benchmarks.py) for benchmarking EigenDB.            
+    The data has been collected using a [simple script](https://github.com/Eigen-DB/eigen-db/blob/main/benchmarks/benchmarks.py) for benchmarking EigenDB.            
     ''')
 
-    col1, col2 = st.columns(2)
-    with col1:
-        generate_line_chart(
-            csv_path='./data/indexing_mean.csv', 
-            title='Mean indexing time',
-            desc='This is the average time to perform similarity search with varying numbers of embeddings and values of k.',
-            x_label='Number of embeddings', 
-            y_label='Mean time (secs)'
-        )
-    with col2:        
-        generate_line_chart(
-            csv_path='./data/indexing_mean.csv',  
-            title='Mean embedding insertion time',
-            desc='This is the average time to insert an embedding with varying numbers of embeddings and values of k.',
-            x_label='Number of embeddings', 
-            y_label='Mean time (secs)'
-        )
+    generate_line_chart(
+        csv_path='./data/indexing_mean.csv', 
+        title='Mean similarity search time',
+        desc='This is the average time to perform similarity search with varying numbers of embeddings and values of k.',
+        x_label='Number of embeddings', 
+        y_label='Mean time (secs)'
+    )    
+    generate_line_chart(
+        csv_path='./data/indexing_mean.csv',  
+        title='Mean embedding insertion time',
+        desc='This is the average time to insert an embedding with varying numbers of embeddings and values of k.',
+        x_label='Number of embeddings', 
+        y_label='Mean time (secs)'
+    )
     
